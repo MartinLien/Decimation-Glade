6ab1c5085f4ff07996e4e5973976f3e1fed90a29
S4r43l <selauvstad@gmail.com>
2023-02-04 23:02:08 +0100

    Added Orb Script

---
+++ Decimation Glade/Assets/Scripts/Orb/OrbController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+
+public class OrbController : MonoBehaviour
+{
+    public Transform RightHand;
+
+    public GameObject playerCamera;
+
+    [SerializeField] private float ThrowForce = 550f;
+    [SerializeField] private float CallBackSpeed = 40f;
+
+    [SerializeField] private float SecondsAnimation;
+    [SerializeField] private float SecondsBeforeCallBack;
+    [SerializeField] bool CastSpell = false;
+    [SerializeField] bool OrbToHand = true;
+    [SerializeField] bool isOrbInHand = true;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (isOrbInHand)
+        {
+            if (CastSpell)
+            {
+                
+                OrbToHand = false;
+                GetComponent<Rigidbody>().useGravity = true;
+                isOrbInHand = false;
+                GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ThrowForce);
+
+               StartCoroutine(delayCallBack());
+               
+            }
+
+        }
+
+        if (OrbToHand)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, RightHand.position, Time.deltaTime * CallBackSpeed);
+            GetComponent<Rigidbody>().useGravity = false;
+
+        }
+    }
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            StartCoroutine(delayAnimationPlaying());
+            
+        }
+    }
+
+
+    IEnumerator delayAnimationPlaying()
+    {
+        yield return new WaitForSeconds(SecondsAnimation);
+        
+        CastSpell = true;
+
+    }
+
+    IEnumerator delayCallBack()
+    {
+        yield return new WaitForSeconds(SecondsBeforeCallBack);
+
+        OrbToHand = true;
+    }
+
+}
